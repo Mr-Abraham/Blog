02db1626cbd928fa2b375f798bd89d8c581101b2
Shivam Abraham <codingnerd84@gmail.com>
2024-05-17 11:33:30 +0530

    installed the dependencies and created the env variables and setup the appwrite project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import conf from "./conf/conf";
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <h1>A Blog App with Appwrite</h1>
+    </>
+  );
+}
+
+export default App;
